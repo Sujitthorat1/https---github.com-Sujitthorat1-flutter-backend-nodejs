56f7f78c374c8bfff503a5cd79cc6987a2e8a4c4
Sujitthorat1 <136691045+Sujitthorat1@users.noreply.github.com>
2023-07-25 18:18:17 +0530

    add-delete-operation

--- src/server.js
+++ src/server.js
@@ -33,8 +33,13 @@ mongoose.connect(uri).then(function () {
         await newNote.save();
         const response = { message: "New note created!" + ` id: ${req.body.id}` };
         res.json(response);
-        
     });
+
+    app.post("/notes/delete", async function (req, res) {
+        await Note.deleteOne({ id: req.body.id });
+        const response = { message: "Note deleted!" + `id:${req.body.id}` }
+        res.json(response)
+    })
 });
 
 // Starting the server on port 5000
